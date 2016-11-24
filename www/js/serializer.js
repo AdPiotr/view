1e92a9d4a0b7126822be76cbbd249fa472eda4f1
Fun Maker <funmaker95@gmail.com>
2016-11-24 17:08:38 +0100

    Proper serializer

--- www/js/serializer.js
+++ www/js/serializer.js
@@ -4,10 +4,63 @@ angular.module('starter.serializer', [])
 
 	return {
 		fromBytes: function(buffer) {
-            JSON.parse(buffer.toString('utf8'))
+			var ret = {}
+			var pos = 0
+            var typ = buffer.readInt8(pos)
+			pos += 1;
+			ret.bodyLength = buffer.readInt32LE(pos)
+			pos += 4;
+			if(typ == 0){
+				ret.textLength = buffer.readInt32LE(pos)
+				pos += 4;
+				ret.text = buffer.toString('utf8', pos, pos + ret.textLength)
+				pos += ret.textLength;
+				ret.type = "text"
+			}else{
+				ret.fileNameLength = buffer.readInt32LE(pos)
+				pos += 4;
+				ret.fileName = buffer.toString('utf8', pos, pos + ret.fileNameLength)
+				pos += ret.fileNameLength;
+				ret.fileSize = buffer.readInt32LE(pos)
+				pos += 4;
+				ret.fileContent = Buffer.from(buffer.slice(pos, pos + ret.fileSize))
+				pos += ret.fileSize;
+				ret.type = "file"
+			}
+			return ret
 		},
 		toBytes: function(packet) {
-            JSON.stringify(packet)
+			var ret
+			var pos = 0
+			if(packet.type == "text") {
+				var text = Buffer.from(packet.text)
+				var ret = Buffer.alloc(text.length + 9)
+				ret.writeInt8(0, pos)
+				pos += 1;
+				ret.writeInt32LE(ret.length - 5, pos)
+				pos += 4;
+				ret.writeInt32LE(text.length, pos)
+				pos += 4;
+				text.copy(ret, pos)
+				pos += text.length;
+			} else {
+				var fileName = Buffer.from(packet.fileName)
+				var fileContent = packet.fileContent
+				var ret = Buffer.alloc(fileName.length + fileContent.length + 13)
+				ret.writeInt8(1, pos)
+				pos += 1;
+				ret.writeInt32LE(ret.length - 5, pos)
+				pos += 4;
+				ret.writeInt32LE(fileName.length, pos)
+				pos += 4;
+				fileName.copy(ret, pos)
+				pos += fileName.length;
+				ret.writeInt32LE(fileContent.length, pos)
+				pos += 4;
+				fileContent.copy(ret, pos)
+				pos += fileContent.length;
+			}
+			return ret
 		}
 	};
 })
