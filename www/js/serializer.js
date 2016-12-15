61019a040f36c027ad0f26e92c570423bee42158
AdPiotr <bawaiha@gmail.com>
2016-12-15 16:40:33 +0100

    bchat

--- www/js/serializer.js
+++ www/js/serializer.js
@@ -1,9 +1,23 @@
 angular.module('starter.serializer', [])
 
 .factory('Serializer', function() {
+	
+	function ab2str(buf) {
+		return String.fromCharCode.apply(null, new Uint16Array(buf));
+	}
+
+	function str2ab(str) {
+		var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+		var bufView = new Uint16Array(buf);
+		for (var i=0, strLen=str.length; i<strLen; i++) {
+			bufView[i] = str.charCodeAt(i);
+		}
+		return buf;
+	}
 
 	return {
 		fromBytes: function(buffer) {
+			return JSON.parse(ab2str(buffer))
 			var ret = {}
 			var pos = 0
             var typ = buffer.readInt8(pos)
@@ -30,6 +44,7 @@ angular.module('starter.serializer', [])
 			return ret
 		},
 		toBytes: function(packet) {
+			return str2ab(JSON.stringify(packet))
 			var ret
 			var pos = 0
 			if(packet.type == "text") {
